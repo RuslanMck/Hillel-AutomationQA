7ef76cf04182da8f81002a24098bc87802f28c8f
Ruslan Marchuk <mckruslan@gmail.com>
2022-04-30 16:43:15 +0200

    The calculator is completed

---
+++ HomeWork04/src/main/java/divisionError.java
@@ -0,0 +1,26 @@
+public class divisionError {
+
+    boolean zeroCheck(long[] array) {
+        boolean result = false;
+        for (double i : array) {
+            if (i == 0) {
+                result = false;
+                System.out.println("ERROR: division by zero");
+                break;
+            } else result = true;
+        }
+        return result;
+    }
+
+    boolean zeroCheck(double[] array) {
+        boolean result = false;
+        for (double i : array) {
+            if (i == 0) {
+                result = false;
+                System.out.println("ERROR: division by zero");
+                break;
+            } else result = true;
+        }
+        return result;
+    }
+}
---
+++ HomeWork04/src/main/java/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        calculator myCalculator = new calculator();
+        System.out.println(myCalculator.multiply(0,5,5,84));
+    }
+}
